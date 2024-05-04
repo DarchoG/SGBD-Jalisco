cc5223ffc6960c1b14e0582568d659950378d576
Daniel <159450603+DarchoG@users.noreply.github.com>
2024-05-04 02:54:18 -0600

    Update Ventana.py
    Incorporación de las imagenes predefinidas, falta por ajustar sus tamaños.

--- Frontend/Ventana.py
+++ Frontend/Ventana.py
@@ -1,4 +1,5 @@
 from customtkinter import *
+from PIL import Image
 import sys  
 
 class Ventana():
@@ -81,10 +82,11 @@ class Ventana():
         #SubWidgets del Segundo Widget Principal
 
         segundoFrame.columnconfigure(0, weight=1)
-        segundoFrame.columnconfigure(1, weight=0)
-        segundoFrame.columnconfigure(2, weight=1)
+        segundoFrame.columnconfigure(1, weight=1)
         segundoFrame.rowconfigure(0, weight=1)
         segundoFrame.update()
+
+        #Barra Lateral Izquierda
         
         segundoPrimerFrame = CTkFrame(master=segundoFrame, fg_color= self.primerGris, 
                                width = self.obtenerAnchoWidget(segundoFrame, 24),
@@ -92,23 +94,15 @@ class Ventana():
                                corner_radius=0)
         segundoPrimerFrame.grid(row = 0, column = 0, sticky="nswe")
 
-        """"
-
-        segundoPrimerBorde = CTkFrame(master=segundoFrame, fg_color= self.primerGris, 
-                               width = self.obtenerAnchoWidget(None, 100, 2),
-                               height = self.obtenerLargoWidget(segundoFrame, 100),
-                               corner_radius=0,
-                               border_color="White",
-                               border_width= 1)
-        segundoPrimerBorde.grid(row = 0, column = 1, sticky="nswe")
-
-        """
+        #Segemento Derecho (Display)
 
         segundoSegundoFrame = CTkFrame(master=segundoFrame, fg_color=self.colorFondo, 
                                width = self.obtenerAnchoWidget(segundoFrame, 74),
                                height = self.obtenerLargoWidget(segundoFrame, 100),
                                corner_radius=0,)
-        segundoSegundoFrame.grid(row = 0, column = 2, sticky="nswe")
+        segundoSegundoFrame.grid(row = 0, column = 1, sticky="nswe")
+
+        self.graficoPredeterminado(segundoSegundoFrame)
 
         #Tercer widget Principal
     
@@ -122,18 +116,7 @@ class Ventana():
 
         tercerFrame.columnconfigure(0, weight=1)
         tercerFrame.rowconfigure(0, weight=1)
-
-        cajaComandos = CTkTextbox(master=tercerFrame,
-                                  width = self.obtenerAnchoWidget(tercerFrame, 100),
-                                  height = self.obtenerLargoWidget(tercerFrame, 100),
-                                  text_color = "White",
-                                  font=("consolas", 15))
-        
-        cajaComandos.grid(row = 0, column = 0, sticky = "nsew")
-        
-        cajaComandos.insert("0.0", "SGDB Jalisco [Version 1.0]\n\n")
-        cajaComandos.insert("3.0", "¡Hola Bienvenido! Escribe /help\n\n")
-        cajaComandos.insert("5.0", "Usuario> ")
+        self.terminal(tercerFrame)
 
     def establecerNavegacion(self, Frame):
         
@@ -152,6 +135,42 @@ class Ventana():
 
        loggin = CTkButton(Frame, text="Loggin", corner_radius=0)
        loggin.grid(row=0, column=2, sticky = "e")
+
+    def graficoPredeterminado(self, Frame):
+
+        Frame.rowconfigure(0, weight = 0)
+        Frame.rowconfigure(1, weight = 0)
+        Frame.columnconfigure(0, weight = 0)
+        Frame.update()
+
+        Imagen = CTkImage(light_image=Image.open("Imagenes/Stock.png"),
+                          dark_image=Image.open("Imagenes/Stock.png"),
+                          size = (100, 100))
+        
+        imagenLabel = CTkLabel(master = Frame, image=Imagen, text = "", bg_color="blue")
+
+        #imagenLabel.grid(row = 0, column = 0,  sticky="nsew")
+
+        Texto = CTkLabel(master = Frame,
+                                         text = "Selecciona el menu de opciones o interactua con la terminal para analizar datos.",
+                                         corner_radius = 0, bg_color="red"
+                                        )
+
+        #Texto.grid(row = 1, column = 0,  sticky="nsew")
+   
+    def terminal(self, Frame):
+                 
+        cajaComandos = CTkTextbox(master=Frame,
+                                  width = self.obtenerAnchoWidget(Frame, 100),
+                                  height = self.obtenerLargoWidget(Frame, 100),
+                                  text_color = "White",
+                                  font=("consolas", 15))
+        
+        cajaComandos.grid(row = 0, column = 0, sticky = "nsew")
+        
+        cajaComandos.insert("0.0", "SGDB Jalisco [Version 1.0]\n\n")
+        cajaComandos.insert("3.0", "¡Hola Bienvenido! Escribe /help\n\n")
+        cajaComandos.insert("5.0", "Usuario> ")
 
     def opcionesMenu(self, opcion):
         
