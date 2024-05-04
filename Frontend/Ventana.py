562cc7aca606a27a74cd6dfa3746ba16e776300b
Daniel <159450603+DarchoG@users.noreply.github.com>
2024-05-03 18:57:12 -0600

    Update Ventana.py

--- Frontend/Ventana.py
+++ Frontend/Ventana.py
@@ -1,4 +1,5 @@
-from customtkinter import *  
+from customtkinter import *
+import sys  
 
 class Ventana():
 
@@ -57,17 +58,23 @@ class Ventana():
         Ventana.rowconfigure(1, weight = 1)
         Ventana.rowconfigure(2, weight = 1)
 
+        #Primer Widget Principal
+
         primerFrame = CTkFrame(master=Ventana, fg_color="red", 
                                width = self.obtenerAncho(Ventana, 100, self.Ancho),
                                height = self.obtenerLargo(Ventana, 5, self.Largo))
         primerFrame.grid(row = 0, column = 0, sticky="nswe")
+
+        self.establecerNavegacion(primerFrame)
+
+        #Segundo Widget Principal
 
         segundoFrame = CTkFrame(master=Ventana, fg_color="green", 
                                width = self.obtenerAncho(Ventana, 100, self.Ancho),
                                height = self.obtenerLargo(Ventana, 65, self.Largo))
         segundoFrame.grid(row = 1, column = 0, sticky="nswe")
 
-        #SubWidgets
+        #SubWidgets del Segundo Widget Principal
 
         segundoFrame.columnconfigure(0, weight=1)
         segundoFrame.columnconfigure(1, weight=1)
@@ -75,16 +82,66 @@ class Ventana():
         segundoFrame.update()
         
         segundoPrimerFrame = CTkFrame(master=segundoFrame, fg_color="black", 
-                               width = self.obtenerAnchoWidget(segundoFrame, 30),
+                               width = self.obtenerAnchoWidget(segundoFrame, 25),
                                height = self.obtenerLargoWidget(segundoFrame, 100))
         segundoPrimerFrame.grid(row = 0, column = 0, sticky="nswe")
 
         segundoSegundoFrame = CTkFrame(master=segundoFrame, fg_color="white", 
-                               width = self.obtenerAnchoWidget(segundoFrame, 70),
+                               width = self.obtenerAnchoWidget(segundoFrame, 75),
                                height = self.obtenerLargoWidget(segundoFrame, 100))
         segundoSegundoFrame.grid(row = 0, column = 1, sticky="nswe")
+
+        #Tercer widget Principal
     
         tercerFrame = CTkFrame(master=Ventana, fg_color="blue", 
                                width = self.obtenerAncho(Ventana, 100, self.Ancho),
                                height = self.obtenerLargo(Ventana, 30, self.Largo))
         tercerFrame.grid(row = 2, column = 0, sticky="nswe")
+
+    def establecerNavegacion(self, Frame):
+        
+       Frame.rowconfigure(0, weight=0)
+       Frame.columnconfigure(0, weight=0)
+       Frame.columnconfigure(1, weight=0)
+       Frame.columnconfigure(2, weight=0)
+
+       valores = ["Opciones", "Diagramas", "Regresiones", "Intervalos Confianza", "Importar", "Exportar" , "Salir"]
+
+       primerAjuste = CTkOptionMenu(Frame, values=valores, corner_radius=0, command=lambda selection: self.opcionesMenu(selection))
+       primerAjuste.grid(row = 0, column = 0, sticky = "e")
+
+       Usuario = CTkLabel(Frame, text="Usuario   ")
+       Usuario.grid(row=0, column=1, sticky = "e")
+
+       loggin = CTkButton(Frame, text="Loggin", corner_radius=0)
+       loggin.grid(row=0, column=2, sticky = "e")
+
+    def opcionesMenu(self, opcion):
+        
+        if "Diagramas" == opcion:
+
+            pass
+
+        elif "Regresiones" == opcion:
+
+            pass
+
+        elif "Intervalos Confianza" == opcion:
+
+            pass
+
+        elif "Importar" == opcion:
+
+            pass
+
+        elif "Exportar" == opcion:
+
+            pass
+
+        elif "Salir" == opcion:
+
+            sys.exit()
+
+        else:
+
+            print("Caso no Definido")               
